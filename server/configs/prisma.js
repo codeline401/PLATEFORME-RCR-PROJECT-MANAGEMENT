e63c5c67d5d78552fe48793d1cef477161d39028
Francis Randrianarivony <f6randrianarivony@gmail.com>
2026-01-12 15:15:24 +0300

    Fix Prisma config + ESM imports + Neon adapter + deleting vercel.json

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,24 +1,23 @@
-import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
 
-import ws from "ws";
-neonConfig.webSocketConstructor = ws;
+neonConfig.poolQueryViaFetch = true;
 
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
 
-// Type definitions
-// declare global {
-//   var prisma: PrismaClient | undefined
-// }
+const globalForPrisma = globalThis;
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
-const adapter = new PrismaNeon({ connectionString });
-const prisma = global.prisma || new PrismaClient({ adapter });
-
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
